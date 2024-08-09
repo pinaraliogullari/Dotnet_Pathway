d8675817786604a9b048a9ed62bfd6e577ee5e33
Pınar ALİOĞULLARI <aliogullarpnar@gmail.com>
2024-08-10 00:17:34 +0300

    In-memory database configuration done.

---
+++ Practices/BookStore/BookStoreWebApi/DbOperations/DataGenerator.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreWebApi.DbOperations
+{
+    public  class DataGenerator
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new BookStoreDbContext(serviceProvider.GetRequiredService<DbContextOptions<BookStoreDbContext>>()))
+            {
+                if (context.Books.Any())
+                {
+                    return;
+                }
+                context.Books.AddRange(
+                        new Book()
+                        {
+                            Id = 1,
+                            Title = "Lean Startup",
+                            GenreId = 1, //PersonalGrowth
+                            PageCount = 200,
+                            PublishDate = new DateTime(2001, 06, 12)
+
+                        },
+                        new Book
+                        {
+                            Id = 2,
+                            Title = "Herland",
+                            GenreId = 2, //Science Fiction
+                            PageCount = 300,
+                            PublishDate = new DateTime(2004, 07, 15)
+
+                        },
+                         new Book
+                         {
+                             Id = 3,
+                             Title = "Dune",
+                             GenreId = 2, //Science Fiction
+                             PageCount = 400,
+                             PublishDate = new DateTime(2010, 05, 23)
+
+                         }
+                    );
+            }
+        }
+    }
+}
--- Practices/BookStore/BookStoreWebApi/Program.cs
+++ Practices/BookStore/BookStoreWebApi/Program.cs
@@ -1,3 +1,6 @@
+using BookStoreWebApi.DbOperations;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<BookStoreDbContext>(options => options.UseInMemoryDatabase(databaseName: "BookStoreDb"));
 
 var app = builder.Build();
 
@@ -22,4 +27,10 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    DataGenerator.Initialize(services);
+}
+    app.Run();

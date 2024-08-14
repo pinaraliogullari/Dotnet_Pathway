95c06bd500b4f7418250e99bbf7214183d5a2ff9
Pınar ALİOĞULLARI <aliogullarpnar@gmail.com>
2024-08-14 15:03:04 +0300

    DeleteBookCommand and DeleteBookValidator classes tested.

---
+++ Practices/BookStore/BookStore/Tests/BookStore.UnitTest/Application/BookOperations/Commands/DeleteBookTest/DeleteBookCommandTest.cs
@@ -0,0 +1,49 @@
+﻿using BookStore.UnitTest.TestSetup;
+using BookStoreWebApi.Application.BookOperations.Commands.DeleteBook;
+using BookStoreWebApi.DbOperations;
+using FluentAssertions;
+using Xunit;
+
+namespace BookStore.UnitTest.Application.BookOperations.Commands.DeleteBookTest
+{
+    public class DeleteBookCommandTest : IClassFixture<CommonTextFixture>
+    {
+        private readonly BookStoreDbContext _context;
+        public DeleteBookCommandTest(CommonTextFixture fixture)
+        {
+            _context = fixture.Context;
+        }
+
+        [Fact]
+        public void WhenBookIsNotFound_InvalidOperationException_ShouldBeReturn()
+        {
+            //arrange
+         
+            DeleteBookCommand command = new(_context);
+            command.BookId = 22;
+
+            //act 
+            var action = FluentActions.Invoking(() => command.Handle());
+
+            //assert
+            action.Should().Throw<InvalidOperationException>().And.Message.Should().Be("Silinecek kitap bulunamadı");
+
+        }
+
+        [Fact]
+
+        public void WhenBookIdIsValid_Book_ShouldBeDeleted()
+        {
+            //arrange
+           DeleteBookCommand command= new(_context);
+            command.BookId = 2;
+
+            //act
+            FluentActions.Invoking(() => command.Handle()).Invoke();
+
+            //assert
+            var book= _context.Books.SingleOrDefault(x=>x.Id==command.BookId);
+            book.Should().BeNull();
+        }
+    }
+}
---
+++ Practices/BookStore/BookStore/Tests/BookStore.UnitTest/Application/BookOperations/Commands/DeleteBookTest/DeleteBookCommandValidatorTest.cs
@@ -0,0 +1,39 @@
+﻿using BookStoreWebApi.Application.BookOperations.Commands.DeleteBook;
+using BookStoreWebApi.ValidationRules;
+using FluentAssertions;
+using Xunit;
+
+namespace BookStore.UnitTest.Application.BookOperations.Commands.DeleteBookTest
+{
+    public class DeleteBookCommandValidatorTest
+    {
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        [InlineData(-100)]
+        [InlineData(-1000)]
+        public void WhenGivenIdIsNotValid_Validator_ShouldBeReturnError(int bookId)
+        {
+            DeleteBookCommand command = new(null);
+            command.BookId = bookId;
+
+            DeleteBookValidator validator = new();
+            var action = FluentActions.Invoking(() => validator.Validate(command)).Invoke();
+
+            action.Errors.Count.Should().BeGreaterThan(0);
+        }
+
+        [Fact]
+        public void WhenGivenIdIsValid_Validator_ShouldBeNotReturnError() 
+        {
+            DeleteBookCommand command = new(null);
+            command.BookId = 1;
+
+            DeleteBookValidator validator = new();
+            var action= FluentActions.Invoking(()=>validator.Validate(command)).Invoke();
+
+            action.Errors.Count().Should().Be(0);
+        }
+    }
+}
